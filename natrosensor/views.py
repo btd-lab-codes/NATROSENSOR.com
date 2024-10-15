24a726541e8b7ee1e2272637718b2d2c42342bcc
jattagaza <jttagaza@gmail.com>
2024-10-15 14:14:13 +0800

    Fixed session issue on process page

--- natrosensor/views.py
+++ natrosensor/views.py
@@ -186,6 +186,9 @@ def process(request):
         request.session['y_int'] = y_int
         request.session['slope'] = slope
 
+        request.session.modified = True
+        return redirect('/result')
+
     template_name = "natrosensor/process.html"
     return render(request, template_name, context={"template_name": "Process", "loc_info": LOC_INFO})
 
